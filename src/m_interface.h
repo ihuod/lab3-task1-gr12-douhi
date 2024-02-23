4a54de1db34c7219a68b56fedd131390ea16525f
Sasha <143936202+ihuod@users.noreply.github.com>
2024-02-23 20:26:26 +0000

    Add countLetter function

--- src/m_interface.h
+++ src/m_interface.h
@@ -2,8 +2,25 @@
 // Created by Alex Douhi 23.02.2024
 //
 
-#ifndef H_INTER
-#define H_INTER
+#ifndef M_INTERFACE_H
+#define M_INTERFACE_H
 
 // Functions listed in this file are used in main.c client file
 // Implementation of these functions is in m_server.c server file
+
+// The countLetter function iterates over each character in the given
+// sentence and compares it with the specified letter. If a match 
+// is found, the count is incremented. The function continues this
+// process until it reaches the end of the sentence. Finally, it 
+// returns the total count of occurrences of the letter in the sentence.
+// Arguments:
+// sentence: A null-terminated string representing the sentence in which
+// to count the occurrences of the letter.
+// letter: The letter to count in the sentence.
+// Return Value:
+// The function returns an integer value representing the number of
+// occurrences of the specified letter in the sentence.
+
+int countLetter(char sentence[], char letter);
+
+#endif
--- src/m_server.c
+++ src/m_server.c
@@ -3,3 +3,32 @@
 //
 
 // Implementation of m_interface.h functions
+
+// The countLetter function iterates over each character in the given
+// sentence and compares it with the specified letter. If a match 
+// is found, the count is incremented. The function continues this
+// process until it reaches the end of the sentence. Finally, it 
+// returns the total count of occurrences of the letter in the sentence.
+// Arguments:
+// sentence: A null-terminated string representing the sentence in which
+// to count the occurrences of the letter.
+// letter: The letter to count in the sentence.
+// Return Value:
+// The function returns an integer value representing the number of
+// occurrences of the specified letter in the sentence.
+
+#include "m_interface.h"
+
+int countLetter(char sentence[], char letter) {
+    int count = 0;
+    int i = 0;
+
+    while (sentence[i] != '\0') {
+        if (sentence[i] == letter) {
+            count++;
+        }
+        i++;
+    }
+
+    return count;
+}
